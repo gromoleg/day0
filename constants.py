1934b76b2c6b5a4d0a71a31c4f5cc390f5c271f4
Oleg Gromyak <oagromyak@gmail.com>
2016-07-18 11:47:48 +0300

    Add abstract API classes

--- constants.py
+++ constants.py
@@ -1,1 +1,3 @@
+MAX_GROUP_MEMBERS = None  # limits GroupAPI.get_members to get only MAX_GROUP_MEMBERS (if present)
+
 MAX_CACHE_SIZE = 10
---
+++ helpers/pclass.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import abc
+import constants
+
+
+class Group(metaclass=abc.ABCMeta):
+    __slots__ = ["id"]
+
+    @abc.abstractclassmethod
+    def get_members(self, count, offset=0, limit=constants.MAX_GROUP_MEMBERS, **options):
+        """
+
+        :rtype: Users
+        """
+        pass
+
+
+class Users(metaclass=abc.ABCMeta):
+    __slots__ = ["ids"]
+
+    @abc.abstractclassmethod
+    def get_information(self, **options):
+        """
+
+        :rtype: list of User objects
+        """
+        pass
+
+
+class User(metaclass=abc.ABCMeta):
+    __slots__ = ["id"]
+
+    @abc.abstractclassmethod
+    def get_information(self, **options):
+        pass
+
+    @abc.abstractclassmethod
+    def get_audio(self, **options):
+        pass
+
+    @abc.abstractclassmethod
+    def get_wall(self, **options):
+        pass
+
+    @abc.abstractclassmethod
+    def get_friends(self, **options):
+        pass
+
+    @abc.abstractclassmethod
+    def get_groups(self, **options):
+        pass
