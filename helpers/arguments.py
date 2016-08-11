488f6de29475ed7aa9f26ea0c0828fb3885d781e
Oleg Gromyak <oagromyak@gmail.com>
2016-08-11 17:38:25 +0300

    Add defaults in help

--- helpers/arguments.py
+++ helpers/arguments.py
@@ -1,7 +1,8 @@
 import argparse
 import constants as c
 
-parser = argparse.ArgumentParser(description='This is ShevIT script by gromoleg.')
+parser = argparse.ArgumentParser(description='This is ShevIT script by gromoleg.',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-g', '--group', help='Group id', required=False,
                     default=c.DEFAULT_GROUP_ID)
 parser.add_argument('-t', '--time', help='Update frequency in minutes', required=False,
